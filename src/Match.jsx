03dc5af8a57b6e22e0d4f1c2025999163f5f818c
MarkusVeesaar <markus.veesaar@voco.ee>
2026-03-24 19:54:48 +0000

    MatchMatch_shuffle_logic_and_IMAGES

--- src/Match.jsx
+++ src/Match.jsx
@@ -1,40 +1,51 @@
 import React from "react";
 import './App.css';
+import { useState } from "react";
+
 
 function Match() {
+
+  const [clicked, setClicked] = useState(true);
+
+  const IMAGES = [
+    "/Match_Photos/Css.png",
+    "/Match_Photos/Rust.png",
+    "/Match_Photos/Ruby.png",
+    "/Match_Photos/Html.png",
+    "/Match_Photos/Java.png",
+    "/Match_Photos/C++.png",
+    "/Match_Photos/React.png",
+    "/Match_Photos/Python.png",
+    "/Match_Photos/Php.png",
+    "/Match_Photos/JavaScript.png"
+  ];
+
+  // Create pairs for matching game (each image appears twice)
+  const gameImages = [...IMAGES, ...IMAGES];
+
+  // Shuffle the images
+  const shuffledImages = [...gameImages].sort(() => Math.random() - 0.5);
+
   return (
     <>
+
+
 
       <div className="Game-match">
         <h1>Match Match</h1>
         <p>Select the Same cards</p>
 
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <br />
+        <div>
+          {shuffledImages.map((image, index) => (
+            <button
+              key={index}
+              onClick={() => setClicked(false)}
+            >
+              {clicked ? <img src={image} /> : "Flip"}
+            </button>
+          ))}
+        </div>
 
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <br />
-
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <br />
-
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
         <br />
         <br />
 
